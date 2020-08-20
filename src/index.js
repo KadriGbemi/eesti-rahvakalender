d8f07657f2a6f029f3e46450507562e77e599750
kadri <kge.sportcompass.dk>
2020-08-20 15:35:04 +0300

    Added redux template and custom files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import CalendarPage from './pages/Calendar';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function Calendar() {
+  return (
+    <div className="calendar-page">
+      <header className="App-header">
+      <Button variant="primary">Primary</Button>
+      </header>
+    </div>
+  );
+}
+
+export default Calendar;
