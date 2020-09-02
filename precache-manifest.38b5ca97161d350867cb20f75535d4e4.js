682634fafc334952d84f8e842e7a8dc1007804a1
kadri <kge.sportcompass.dk>
2020-09-02 18:05:14 +0300

    Updates

---
+++ precache-manifest.38b5ca97161d350867cb20f75535d4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb68e0d4ba3b0ac27c41f3e984fdffe8",
+    "url": "/events-calendar/index.html"
+  },
+  {
+    "revision": "72d512402390faee0b43",
+    "url": "/events-calendar/static/css/main.1851f714.chunk.css"
+  },
+  {
+    "revision": "f9754c4929cc317fa22f",
+    "url": "/events-calendar/static/js/2.380a40c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/events-calendar/static/js/2.380a40c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d512402390faee0b43",
+    "url": "/events-calendar/static/js/main.84475854.chunk.js"
+  },
+  {
+    "revision": "4dd604c277883437e056",
+    "url": "/events-calendar/static/js/runtime-main.870400ac.js"
+  }
+]);
